9a1a3cc3259ff36f0dbe2c631f851040fb5e5799
Devop-A <hunter.austin407@gmail.com>
2024-01-01 20:23:29 -0500

    fix git commit -m fix
    git commit -m fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+333
+33
+33
+33
+33
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
